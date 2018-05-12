de7bbbd48de71bd7b022c45c5cec9a6d1be85fd9
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-12 21:30:49 +0300

    Items are now stored and read from sessionStorage

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@
   const addButton = document.querySelector('.todo-form__button');
 
   const helper = (function() {
+    const _storageType = sessionStorage;
+    const _storageKey = 'muh_todos';
     function escape(input) {
       return input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;');
     }
@@ -22,8 +24,40 @@
       return li;
     }
 
+    function accessStorage() {
+      let items;
+      if (_storageType.getItem(_storageKey) === null) {
+        items = [];
+      } else {
+        items = JSON.parse(_storageType.getItem(_storageKey));
+      }
+      return items;
+    }
+
+    function save(item) {
+      let items = accessStorage();
+      items.push(item);
+      _storageType.setItem(_storageKey, JSON.stringify(items));
+    }
+
+    function load() {
+      let items = accessStorage();
+      return items;
+    }
+
+    function remove(removedItem) {
+      let items = accessStorage();
+      const remainingItems = items.filter(item => {
+        return item.toLowerCase() !== removedItem.toLowerCase();
+      });
+      _storageType.setItem(_storageKey, JSON.stringify(remainingItems));
+    }
+
     return {
-      createTodo
+      createTodo,
+      save,
+      load,
+      remove
     };
   })();
 
@@ -31,6 +65,7 @@
     e.preventDefault();
     if (inputField.value !== '') {
       todoList.appendChild(helper.createTodo(inputField.value));
+      helper.save(inputField.value);
       inputField.value = '';
     }
   };
@@ -38,10 +73,19 @@
   const handleRemoveItem = (e) => {
     const parent = e.target.parentElement;
     if (parent.classList.contains('remove-item')) {
+      helper.remove(parent.parentElement.textContent);
       parent.parentElement.remove();
     }
   };
 
+  const handleInitialLoad = (e) => {
+    const items = helper.load();
+    items.forEach(item => {
+      todoList.appendChild(helper.createTodo(item));
+    });
+  };
+
   addButton.addEventListener('click', handleAddButton);
   todoList.addEventListener('click', handleRemoveItem);
+  document.addEventListener('DOMContentLoaded', handleInitialLoad);
 }());
