f210e95d719349ffc65093113f6dcbc005f4c358
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-12 20:52:20 +0300

    Refactor code and change icons

--- src/app.js
+++ src/app.js
@@ -5,23 +5,42 @@
   const inputField = document.querySelector('.todo-form__input');
   const addButton = document.querySelector('.todo-form__button');
 
-  // TODO: Is this enough?
-  const escape = (input) => {
-    return input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;');
-  };
+  const helper = (function() {
+    function escape(input) {
+      return input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;');
+    }
+
+    function createTodo(text) {
+      const li = document.createElement('li');
+      li.classList.add('todo-list__item');
+      li.appendChild(document.createTextNode(escape(text)));
+
+      const del = document.createElement('a');
+      del.className = 'delete-item';
+      del.innerHTML = '<i class="fa fa-trash"></i>';
+      li.appendChild(del);
+      return li;
+    }
+
+    return {
+      createTodo
+    };
+  })();
 
   const handleAddButton = (e) => {
     e.preventDefault();
-
     if (inputField.value !== '') {
-      const li = document.createElement('li');
-      li.classList.add('todo-list__item');
-      li.innerHTML = `${escape(inputField.value)} <i class="fa fa-times"></i>`;
-      todoList.appendChild(li);
-
+      todoList.appendChild(helper.createTodo(inputField.value));
       inputField.value = '';
     }
   };
 
+  const handleRemoveItem = (e) => {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+      console.log('trash');
+    }
+  };
+
   addButton.addEventListener('click', handleAddButton);
+  todoList.addEventListener('click', handleRemoveItem);
 }());
